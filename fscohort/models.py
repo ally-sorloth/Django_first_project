6901c299e9346989d0ea57035fc4d6cc27d5b5aa
ally-sorloth <allysorloth@gmail.com>
2020-12-24 23:56:08 +0300

    change models

--- fscohort/models.py
+++ fscohort/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
-    first_name = models.CharField(max_length=50)
+    sir_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     number = models.IntegerField()
+    
+    def __str__(self):
+        return self.sir_name
